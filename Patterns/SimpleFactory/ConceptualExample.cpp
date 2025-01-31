440c2f2c89ad6ed2f61d4ab6f89799c9d0b9af24
Peter Loos <peter.loos@gmx.de>
2025-01-31 10:59:08 +0100

    Add project files.

---
+++ Patterns/SimpleFactory/ConceptualExample.cpp
@@ -0,0 +1,135 @@
+// ===========================================================================
+// ConceptualExample.cpp // Factory a.k.a. Simple Factory
+// ===========================================================================
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+namespace ConceptualExample {
+
+    class IShape
+    {
+    public:
+        virtual ~IShape() {}
+
+        virtual void draw() = 0;
+    };
+
+    class Rectangle : public IShape 
+    {
+    public:
+        Rectangle() {}
+
+        void draw() override
+        {
+            std::cout << "> Rectangle::draw()" << std::endl;
+        }
+    };
+
+    class Square : public IShape 
+    {
+    public:
+        Square() {}
+
+        void draw() override
+        {
+            std::cout << "> Square::draw()" << std::endl;
+        }
+    };
+
+    class Circle : public IShape 
+    {
+    public:
+        Circle() {}
+
+        void draw() override
+        {
+            std::cout << "> Circle ::draw()" << std::endl;
+        }
+    };
+
+    // ===================================================
+
+    class ShapeFactory
+    {
+    public:
+        enum class ShapeKind
+        {
+            Rectangle,
+            Square,
+            Circle,
+            None
+        };
+
+        std::shared_ptr<IShape> getShape(ShapeKind shapeType)
+        {
+            std::shared_ptr<IShape> shapePtr;
+
+            switch (shapeType)
+            {
+            case ShapeKind::Rectangle:
+                shapePtr = std::make_shared<Rectangle>();
+                break;
+
+            case ShapeKind::Square:
+                shapePtr = std::make_shared<Square>();
+                break;
+
+            case ShapeKind::Circle:
+                shapePtr = std::make_shared<Circle>();
+                break;
+
+            default:
+                std::string msg = "Invalid Shape Kind";
+                throw std::runtime_error{ msg };
+            }
+
+            return shapePtr;
+        }
+    };
+
+    static void test_without_factory()
+    {
+        // using classes explicitely
+        //
+        Rectangle* rect{ new Rectangle{} };
+        rect->draw();
+
+        Circle* circle{ new Circle{} };
+        circle->draw();
+
+        Square* square{ new Square{} };
+        square->draw();
+    }
+
+    static void test_with_factory()
+    {
+        // using classes indirectly with factory
+        // (knowledge about explicit classes not necessary)
+        // 
+        ShapeFactory shapeFactory;
+
+        std::shared_ptr<IShape> shape{};  // Interface Pointer
+
+        shape = shapeFactory.getShape(ShapeFactory::ShapeKind::Circle);
+        shape->draw();
+
+        shape = shapeFactory.getShape(ShapeFactory::ShapeKind::Rectangle);
+        shape->draw();
+
+        shape = shapeFactory.getShape(ShapeFactory::ShapeKind::Square);
+        shape->draw();
+    }
+}
+
+void test_conceptual_example ()
+{
+    using namespace ConceptualExample;
+    test_without_factory();
+    test_with_factory();
+}
+
+// ===========================================================================
+// End-of-File
+// ===========================================================================
---
+++ Patterns/Singleton/ConceptualExample01.cpp
@@ -0,0 +1,114 @@
+// ===========================================================================
+// ConceptualExample01.cpp
+// ===========================================================================
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+namespace ConceptualExample01 {
+
+    class Singleton final
+    {
+    private:
+        static Singleton* m_instance;
+        static std::mutex m_mutex;
+
+    private:
+        Singleton() = default;
+
+        // no copying
+        Singleton(const Singleton&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+
+        // no moving
+        Singleton(Singleton&&) noexcept = delete;
+        Singleton& operator=(Singleton&&) noexcept = delete;
+
+    public:
+        static Singleton* getInstance()   // easy
+        {
+            if (m_instance == nullptr) {
+                m_instance = new Singleton{};   // Heap
+            }
+
+            return m_instance;
+        }
+
+        static Singleton* getInstanceThreadSafe()
+        {
+            // klassisch
+            m_mutex.lock();
+
+            if (m_instance == nullptr) {
+                // thread wechsel
+                m_instance = new Singleton{};  // std::bad_alloc
+            }
+            
+            m_mutex.unlock();
+
+            // versus
+
+            {
+                // Sperre   // RAII Idiom  // Hüllenobjekt
+                std::lock_guard<std::mutex> lock{ m_mutex };  // .lock()
+
+                if (m_instance == nullptr) {
+                    // thread wechsel
+                    m_instance = new Singleton{};
+                }
+            }   // mutex.unlock();
+
+            return m_instance;
+        }
+
+        static Singleton* getInstanceThreadSafe_Cpp17()
+        {
+            if (std::lock_guard<std::mutex> lock{ m_mutex }; m_instance == nullptr) {
+                m_instance = new Singleton{};
+            }
+
+            return m_instance;
+        }
+
+        void doThis() const
+        {
+            std::cout << "doing this on singleton instance (" << this << ")." << std::endl;
+        }
+
+        void doThat() const
+        {
+            std::cout << "doing that on singleton instance (" << this << ")." << std::endl;
+        }
+    };
+
+    Singleton* Singleton::m_instance{ nullptr };
+    std::mutex Singleton::m_mutex{};
+}
+
+void test_singleton_01()
+{
+    using namespace ConceptualExample01;
+
+    Singleton::getInstance()->doThis();
+    Singleton::getInstance()->doThat();
+}
+
+void test_singleton_02()
+{
+    using namespace ConceptualExample01;
+
+    auto singleton1 { Singleton::getInstance() };
+    auto singleton2 { Singleton::getInstance() };
+
+    std::cout << std::boolalpha << (singleton1 == singleton2) << std::endl;
+
+    auto singleton3 { Singleton::getInstanceThreadSafe() };
+    auto singleton4 { Singleton::getInstanceThreadSafe() };
+
+    std::cout << std::boolalpha << (singleton3 == singleton4) << std::endl;
+}
+
+// ===========================================================================
+// End-of-File
+// ===========================================================================
